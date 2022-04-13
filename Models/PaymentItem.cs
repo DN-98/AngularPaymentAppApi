95478b73185119ea74b4da593bfb1493f1cb1476
DN-98 <deahnisadna@gmail.com>
2022-04-13 10:36:22 +0700

    first commit

---
+++ Models/PaymentItem.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentItem{
+        [Key]
+        public int paymentDetailId {get; set;}
+        [Required]
+        public string cardOwnerName {get; set;}
+        [Required]
+        public string cardOwnerNumber {get; set;}
+        [Required]
+        public DateTime expirationDate {get; set;}
+        [Required]
+        public string securityCode {get; set;}
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>Payment API</h1>
+<h3>What's API</h3>
+<p>An application programming interface (API) is a connection between computers or between computer programs. It is a type of software interface, offering a service to other pieces of software. (source: Wikipedia)</p>
+<p>This is An example API for payment and has been deployed to Heroku</p>
+https://deah-payment-api.herokuapp.com/
+<p>This API build using ASP.NET Core Web API, swagger, tailwind css
